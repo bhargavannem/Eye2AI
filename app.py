eb8e5c1ae88143ddc8afde0251deb5c5d4d85c0c
bhargavannem <bhargav.annem@gmail.com>
2021-02-20 22:18:01 -0600

    Fixed graph issue ✅

--- app.py
+++ app.py
@@ -22,24 +22,25 @@ from keras.preprocessing import image
 def predict(filename):
         md = tf.keras.models.load_model('static/my_model.h5')
         image_dims = 160
-        DIR = os.path.join('static/uploads', filename)
+        DIR = os.path.join('static/images', filename)
         test_image = load_img(DIR,target_size=(image_dims, image_dims))
-        
+
         test_image = image.img_to_array(test_image)
         test_image = np.expand_dims(test_image, axis=0)
         test_image /= 255
         result = md.predict(test_image)
-        
+
         maxValue= -1
         for i in result[0]:
                 if i > maxValue:
                         maxValue = i
 
-        
+
+        print(result[0] * 100)
         return result[0] * 100
 
 app = Flask(__name__,template_folder='templates')
-app.config['IMAGE_UPLOADS'] = "static/uploads"
+app.config['IMAGE_UPLOADS'] = "static/images"
 app.config['ALLOWED_IMAGE_EXTENSIONS'] = ['JPEG', 'JPG', 'PNG']
 
 def allowed_image(filename):
@@ -59,17 +60,17 @@ def home():
         if request.files:
             image = request.files["image"]
             if image.filename == "":
-            
+
                 return redirect(request.url)
 
             if not allowed_image(image.filename):
-            
+
                 return redirect(request.url)
 
             else:
                 filename = secure_filename(image.filename)
                 image.save(os.path.join(app.config["IMAGE_UPLOADS"], filename))
-            
+
             prd = predict(filename)
             os.remove(os.path.join(app.config["IMAGE_UPLOADS"],filename))
             return render_template('index.html',cnv=prd[0], dme=prd[1], drusen=prd[2], normal=prd[3], prediction=True)
@@ -77,4 +78,4 @@ def home():
         return render_template('index.html')
 
 if __name__ == "__main__":
-        app.run()
+    app.run(debug=True)
